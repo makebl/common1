13b2a6cce4433d53467a2c2bf51aaa43cbb416cc
279437541 <78850005+279437541@users.noreply.github.com>
2022-01-24 20:08:28 +0800

    Update AutoUpdate.sh

--- AutoUpdate.sh
+++ AutoUpdate.sh
@@ -2,7 +2,7 @@
 # https://github.com/Hyy2001X/AutoBuild-Actions
 # AutoBuild Module by Hyy2001
 # AutoUpdate for Openwrt
-export CURRENT_Version="/proc/version"
+#export CURRENT_Version="/proc/version"
 Version=V6.5
 
 Shell_Helper() {
