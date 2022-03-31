eeb14a047143adb5e8d491229264f8109ef28efa
马可 <78850005+makebl@users.noreply.github.com>
2022-04-01 01:21:18 +0800

    Update common.sh

--- common.sh
+++ common.sh
@@ -214,7 +214,7 @@ esac
 echo "
 src-git helloworld https://github.com/fw876/helloworld
 src-git passwall https://github.com/shidahuilang/openwrt-passwall
-src-git dahuilang https://github.com/makebl/openwrt-package.git;${REPO_BRANCH}
+src-git makebl https://github.com/makebl/openwrt-package.git;${REPO_BRANCH}
 " >> $HOME_PATH/feeds.conf.default
 sed -i '/^\s*$/d' "$HOME_PATH/feeds.conf.default"
 }
