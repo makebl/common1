8c1d65423df468f96ec36e8ecf45683722c6f2b2
马可 <78850005+makebl@users.noreply.github.com>
2022-07-31 21:43:30 +0800

    Update common.sh

--- common.sh
+++ common.sh
@@ -345,7 +345,7 @@ function Diy_Lede() {
 echo "正在执行：Lede专用自定义"
 cat >>"${KEEPD}" <<-EOF
 /mnt/network
-## /mnt/Detectionnetwork
+/mnt/Detectionnetwork
 /etc/config/AdGuardHome.yaml
 /www/luci-static/argon/background
 EOF
@@ -817,11 +817,14 @@ fi
 
 function Diy_files() {
 echo "正在执行：files大法，设置固件无烦恼"
+
 if [[ -d "${GITHUB_WORKSPACE}/OP_DIY" ]]; then
   cp -Rf $HOME_PATH/build/common/${SOURCE}/* $BUILD_PATH
   cp -Rf ${GITHUB_WORKSPACE}/OP_DIY/${matrixtarget}/* $BUILD_PATH
- elif [[ ${matrixtarget} == "nanopi_r2s" ]]; then
+
+elif [[ ${matrixtarget} == "nanopi_r2s" ]]; then
   cp -Rf $HOME_PATH/build/common/nanopi_r2s/* $BUILD_PATH
+  
 elif [[ ${matrixtarget} == "nanopi_r4s" ]]; then
   cp -Rf $HOME_PATH/build/common/nanopi_r4s/* $BUILD_PATH
 else
