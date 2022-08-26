9d121666b374de4fd8feb85fc1961a04ea89d8f5
马可 <78850005+makebl@users.noreply.github.com>
2022-08-26 22:18:43 +0800

    Update FinishIng.sh

--- Custom/FinishIng.sh
+++ Custom/FinishIng.sh
@@ -1,10 +1,1 @@
 
-# slim 固件本地 opkg 配置
-if ls -l /local_feed/*.ipk &>/dev/null;then
-    sed -ri 's@^[^#]@#&@' /etc/opkg/distfeeds.conf
-    grep -E '/local_feed' /etc/opkg/customfeeds.conf || echo 'src/gz local file:///local_feed' >> /etc/opkg/customfeeds.conf
-    # 取消签名，暂时解决不了
-    sed -ri '/check_signature/s@^[^#]@#&@' /etc/opkg.conf
-fi
-
-exit 0
