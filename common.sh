748e61fc32f19c34d0cb87fb72792eca372078de
马可 <78850005+makebl@users.noreply.github.com>
2022-10-22 18:06:24 +0800

    Update common.sh

--- common.sh
+++ common.sh
@@ -866,10 +866,7 @@ if [[ "${REPO_BRANCH}" == "22.03" ]] || [[ "${REPO_BRANCH}" == "openwrt-21.02" ]
 fi
 }
 
-function Diy_part_sh() {
-cd ${HOME_PATH}
-echo "正在执行：运行$DIY_PART_SH文件"
-source "${BUILD_PATH}/${DIY_PART_SH}"
+
 
 # 修正连接数
 echo -e "\nnet.netfilter.nf_conntrack_max=165535" >>package/base-files/files/etc/sysctl.conf
