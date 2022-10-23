4fbd58b20be83ba2a789c32449d16105036559e4
马可 <78850005+makebl@users.noreply.github.com>
2022-10-23 17:38:50 +0800

    Update common.sh

--- common.sh
+++ common.sh
@@ -43,7 +43,7 @@ elif [[ ${SOURCE_CODE} == "IMMORTAL" ]]; then
   export REPO_BRANCH="openwrt-21.02"
 elif [[ ${SOURCE_CODE} == "TIANLING" ]]; then
   export REPO_URL="https://github.com/immortalwrt/immortalwrt"
-  export REPO_BRANCH="openwrt-18.06"
+  export REPO_BRANCH="openwrt-18.06-k5.4"
 else
   TIME r "没有发现该源码,或者源码获取方法已更改,请同步上游仓库，或者重新拉取上游仓库，特别留意settings.ini的更改"
   exit 1
