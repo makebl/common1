105fbbd6a4f18b08a2effb4ec0be18827c524025
马可 <78850005+makebl@users.noreply.github.com>
2022-11-08 22:49:54 +0800

    Update common.sh

--- common.sh
+++ common.sh
@@ -1072,7 +1072,7 @@ echo "${Plug_in2}" >Plug-in
 sed -i '/luci-app-qbittorrent-simple_dynamic/d' Plug-in > /dev/null 2>&1
 
 sed -ri 's@^[^#]@#&@' /etc/opkg/distfeeds.conf
-grep -E '/local_feed' /etc/opkg/customfeeds.conf || echo 'src/gz src/gz openwrt_kiddin9 https://op.supes.top/packages/x86_64' >> /etc/opkg/customfeeds.conf
+grep -E '/local_feed' /etc/opkg/customfeeds.conf || echo 'src/gz lang https://op.supes.top/packages/x86_64' >> /etc/opkg/customfeeds.conf
 # 取消签名，暂时解决不了
 sed -ri '/check_signature/s@^[^#]@#&@' /etc/opkg.conf
 
