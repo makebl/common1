d9f3263e76ac842533fec15a6246f719eb7593b8
马可 <78850005+makebl@users.noreply.github.com>
2022-11-09 18:06:33 +0800

    Update common.sh

--- common.sh
+++ common.sh
@@ -369,9 +369,8 @@ src-git helloworld https://github.com/fw876/helloworld
 src-git passwall https://github.com/xiaorouji/openwrt-passwall;packages
 src-git passwall1 https://github.com/xiaorouji/openwrt-passwall;luci
 src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2;main
-src-git small8 https://github.com/kenzok8/small-package;${REPO_BRANCH}
-#src-git nas https://github.com/linkease/nas-packages.git;master
-#src-git nas_luci https://github.com/linkease/nas-packages-luci.git;main
+src-git kiddin9 https://github.com/kiddin9/openwrt-packages;${REPO_BRANCH}
+
 " >> ${HOME_PATH}/feeds.conf.default
 sed -i '/^#/d' "${HOME_PATH}/feeds.conf.default"
 sed -i '/^$/d' "${HOME_PATH}/feeds.conf.default"
