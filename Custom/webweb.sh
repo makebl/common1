44113cf021c9ca626fd4df0c8e7bc73c5c604c88
马可 <78850005+makebl@users.noreply.github.com>
2022-11-11 18:17:19 +0800

    Update webweb.sh

--- Custom/webweb.sh
+++ Custom/webweb.sh
@@ -35,8 +35,8 @@ if [[ -f /etc/config/argon ]]; then
   uci commit argon
 fi
 
-if [[ `grep -c "shidahuilang" /etc/opkg/distfeeds.conf` -ge '1' ]]; then
-  sed -i '/shidahuilang/d' /etc/opkg/distfeeds.conf
+if [[ `grep -c "dahuilang" /etc/opkg/distfeeds.conf` -ge '1' ]]; then
+  sed -i '/dahuilang/d' /etc/opkg/distfeeds.conf
 fi
 if [[ `grep -c "helloworld" /etc/opkg/distfeeds.conf` -ge '1' ]]; then
   sed -i '/helloworld/d' /etc/opkg/distfeeds.conf
