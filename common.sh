59acb2be8557c074a729eac3c1b29f88ed981d4f
马可 <78850005+makebl@users.noreply.github.com>
2023-07-26 00:20:47 +0800

    Update common.sh

--- common.sh
+++ common.sh
@@ -1441,18 +1441,18 @@ else
   weizhicpu="1"
 fi
 
-if [[ ! "${weizhicpu}" == "1" ]] && [[ -n "${OpenClash_Core}" ]] && [[ "${OpenClash_branch}" =~ (Lede|dev) ]]; then
+if [[ ! "${weizhicpu}" == "1" ]] && [[ -n "${OpenClash_Core}" ]] && [[ "master" =~ (master|Lede|dev) ]]; then
   echo "正在执行：给openclash下载核心"
   rm -rf ${HOME_PATH}/files/etc/openclash/core
   rm -rf ${HOME_PATH}/clash-neihe && mkdir -p ${HOME_PATH}/clash-neihe
   mkdir -p ${HOME_PATH}/files/etc/openclash/core
   cd ${HOME_PATH}/clash-neihe
   if [[ "${OpenClash_Core}" == "2" ]]; then
-    wget -q https://raw.githubusercontent.com/vernesong/OpenClash/core/${OpenClash_branch}/meta/clash-${Archclash}.tar.gz -O meta.tar.gz
-    wget -q https://raw.githubusercontent.com/vernesong/OpenClash/core/${OpenClash_branch}/dev/clash-${Archclash}.tar.gz -O clash.tar.gz
-    wget -q https://raw.githubusercontent.com/vernesong/OpenClash/core/${OpenClash_branch}/core_version -O core_version
+    wget -q https://raw.githubusercontent.com/vernesong/OpenClash/core/master/meta/clash-${Archclash}.tar.gz -O meta.tar.gz
+    wget -q https://raw.githubusercontent.com/vernesong/OpenClash/core/master/dev/clash-${Archclash}.tar.gz -O clash.tar.gz
+    wget -q https://raw.githubusercontent.com/vernesong/OpenClash/core/master/core_version -O core_version
     TUN="$(cat core_version |grep -v "^v\|^V\|^a" |grep -E "[0-9]+.[0-9]+.[0-9]+")"
-    wget -q https://raw.githubusercontent.com/vernesong/OpenClash/core/${OpenClash_branch}/premium/clash-${Archclash}-${TUN}.gz -O clash_tun.gz
+    wget -q https://raw.githubusercontent.com/vernesong/OpenClash/core/master/premium/clash-${Archclash}-${TUN}.gz -O clash_tun.gz
     
     tar -zxvf clash.tar.gz -O > clash
     if [[ $? -eq 0 ]];then
