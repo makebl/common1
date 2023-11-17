bebcc288f1d16d1643a7369a17da358747b7fc09
马可 <78850005+makebl@users.noreply.github.com>
2023-11-17 20:46:58 +0800

    Update upgrade.sh

--- upgrade.sh
+++ upgrade.sh
@@ -46,9 +46,7 @@ function Diy_Part2() {
 	ramips | reltek | ath* | ipq* | bcm47xx | bmips | kirkwood | mediatek)
 		export Firmware_SFX=".bin"
 		export AutoBuild_Firmware="${LUCI_EDITION}-${SOURCE}-${TARGET_PROFILE_ER}-${Upgrade_Date}-sysupgrade"
-                export AutoBuild_Firmware="${LUCI_EDITION}-${SOURCE}-${TARGET_PROFILE_ER}-${Upgrade_Date}-initramfs-kernel"
-		export AutoBuild_Firmware="${LUCI_EDITION}-${SOURCE}-${TARGET_PROFILE_ER}-${Upgrade_Date}-squashfs-kernel1"
-                export AutoBuild_Firmware="${LUCI_EDITION}-${SOURCE}-${TARGET_PROFILE_ER}-${Upgrade_Date}-squashfs-rootfs0"
+
 	;;
 	x86)
 		export Firmware_SFX=".img.gz"
@@ -78,9 +76,7 @@ function Diy_Part2() {
 	*)
 		export Firmware_SFX=".bin"
 		export AutoBuild_Firmware="${LUCI_EDITION}-${SOURCE}-${TARGET_PROFILE_ER}-${Upgrade_Date}-sysupgrade"
-                export AutoBuild_Firmware="${LUCI_EDITION}-${SOURCE}-${TARGET_PROFILE_ER}-${Upgrade_Date}-initramfs-kernel"
-		export AutoBuild_Firmware="${LUCI_EDITION}-${SOURCE}-${TARGET_PROFILE_ER}-${Upgrade_Date}-squashfs-kernel1"
-                export AutoBuild_Firmware="${LUCI_EDITION}-${SOURCE}-${TARGET_PROFILE_ER}-${Upgrade_Date}-squashfs-rootfs0"
+
 	;;
 	esac
 	
