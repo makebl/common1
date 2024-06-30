77d32234ac1888578711eba2bc62088237f4c0cb
马可 <78850005+makebl@users.noreply.github.com>
2024-06-30 15:11:14 +0800

    Update common.sh

--- common.sh
+++ common.sh
@@ -209,7 +209,6 @@ echo "Compte_Date=$(date +%Y年%m月%d号%H时%M分)" >> ${GITHUB_ENV}
 echo "Tongzhi_Date=$(date +%Y年%m月%d日)" >> ${GITHUB_ENV}
 echo "Gujian_Date=$(date +%m.%d)" >> ${GITHUB_ENV}
 echo "FILE_TIME=$(date +"%m-%d %H.%M")" >> $GITHUB_ENV
-echo "FILE_TIME1=$(date "+%Y年%m月%d日-%H点%M分")" >> $GITHUB_ENV
 echo "FILE_TIME2=$(date "+%Y%m%d-%H%M")" >> $GITHUB_ENV
 echo "START_SECONDS=$(date --date="$START_TIME" +%s)" >> $GITHUB_ENV
 if [[ -n "${BENDI_VERSION}" ]]; then
